8253a0290bbf8be1758c3635f4663c23906eb90b
zuoez02 <zuoez02@outlook.com>
2022-11-18 19:07:12 +0800

    First commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import 'mathjax/es5/output/chtml/fonts/woff-v2/MathJax_AMS-Regular.woff'
+
+createApp(App).mount('#app')
